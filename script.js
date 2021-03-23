704b6ddcd7cd02bd508f3c65d9218e9ff2f0f469
Joy Ho <joyho796@gmail.com>
2021-03-23 18:18:37 +0800

    change nav direction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("mobileNav").style.width = "100%";
+  document.getElementById("mobileNav").style.height = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("mobileNav").style.width = "0%";
+  document.getElementById("mobileNav").style.height = "0%";
 }
