be7a68ecb432e2a95913535a0bb538b1f0783489
Joy Ho <joyho796@gmail.com>
2021-03-23 21:22:18 +0800

    mainly store css

--- store_script.js
+++ store_script.js
@@ -1,3 +1,33 @@
+function animate(div) {
+    div.animate({height: '100px'}, 1000);
+    setTimeout(function() {
+        div.val("gathering your mystery boxes...");
+    },10);
+    div.animate({width: '80%'}, 3000);
+    div.animate({height: '50px'}, "slow");
+    div.animate({width: '200px'}, "slow");
+    setTimeout(function() {
+        div.val("Order Complete");
+    },5010);
+    setTimeout(function() {
+        div.val("Confirm Order");
+    },6000);
+
+}
+
+function DonateItem(name, cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+
+        donate_items = new Array(
+            new DonateItem("Meal Box", 7),
+            new DonateItem("Toy", 5),
+            new DonateItem("Care Coupon", 11),
+            new DonateItem("Merch Item", 15),
+        );
+
 window.onload = function() {
 
     for (i = 0; i < donate_items.length; i++)
@@ -35,21 +65,19 @@ window.onload = function() {
             quant = parseInt(document.getElementsByName("quantity")[i].value);
             if(quant != 0){
                 number = donate_items[i].cost * quant;
-                temp = "<p>"+ quant + " " +donate_items[i].name+" = $"+number+"</p>"
+                temp = "<p>"+ quant + " " +donate_items[i].name;
+                if(quant>1){
+                    temp +="s";
+                }
+                temp +=" = $"+number+"</p>"
                 summary += temp;
             }
         }
         return summary;
     }
 
-    $("#button").click(function(){
-
-        var div = $("#button");
-        div.animate({height: '300px', opacity: '0.4'}, "slow");
-        div.animate({width: '300px', opacity: '0.8'}, "slow");
-        div.animate({height: '50px', opacity: '0.4'}, "slow");
-        div.animate({width: '180px', opacity: '0.8'}, "slow");
-
+    $("#button").click(function()
+    {
         var submitAccept = true;
         var errString ="Please correct the following errors:\n"
         var email_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -62,33 +90,38 @@ window.onload = function() {
         }
         if(count == donate_items.length){
             submitAccept = false;
-            errString += "- Please enter a non-zero quantity for atleast one item.\n";
+            errString += "    - You must order at least one item\n";
         }
 
         if(document.getElementById("name").value==""){
             submitAccept = false;
-            errString += "- Please enter your name.\n";
+            errString += "    - Name cannot be blank\n";
         }
 
         if(document.getElementById("email").value==""){
             submitAccept = false;
-            errString += "- Please enter your email ID.\n";
+            errString += "    - Email cannot be blank\n";
+        }
+        if(document.getElementById("recip").value==""){
+            submitAccept = false;
+            errString += "    - Please indicate who or what kind of pet the recipient is\n";
         }
         else if (!(document.getElementById("email").value.match(email_regex))){
             submitAccept = false;
-            errString += "- Please enter a valid email ID.\n";
+            errString += "    - Email must be valid\n";
         }
 
         if(!(document.getElementById("phone").value=="")){
             if(!(document.getElementById("phone").value.match(ph_regex))){
                 submitAccept = false;
-                errString += "- Please enter a valid phone numnber.\n";
+                errString += "    - Phone number must be valid\n";
             }
         }
 
         if(submitAccept == false){
             alert(errString)
-        }else{
+        } else{
+            animate($("#button"));
             order_total = 0;
             orderSummary = "Order Summary:\n";
             for (i = 0; i < donate_items.length; i++)
@@ -97,16 +130,24 @@ window.onload = function() {
                 if(quant != 0){
                     number = donate_items[i].cost * quant;
                     order_total += number
-                    temp = quant + " " +donate_items[i].name+" = $"+number+"\n"
+                    temp = quant + " " +donate_items[i].name
+                    if(quant>1){
+                        temp += "s";
+                    }
+                    temp += " = $"+number+"\n"
                     orderSummary += temp;
                 }
             }
             order_total = 1.0625 * order_total;
-            alert("\nThanks for your donation.\n\n" +
+            setTimeout(function() {
+                alert("\nThanks you for your order.\n\n" +
                 orderSummary + "\n" +
-                "Total Amount: $" + order_total + "\n\n" +
-                "We really appreciate your contribution!"
-                )
+                "Total Amount: $" + order_total.toFixed(2) + "\n\n" +
+                "We will email you with more information about payment details. We, and our animals, appreciate your support!"
+                );
+            },5500);
+
         }
+
     });
 };
